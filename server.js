bb238cd5144bcd0fe2b18cfd6320da4c8d176ad6
Paulo Marcelo <paulomarceloa.nogueira@gmail.com>
2019-09-18 14:42:28 -0300

    Remove comments

--- server.js
+++ server.js
@@ -6,25 +6,3 @@ const apiController = new ApiController();
 apiController.run(() => {
   console.log('Api is running');
 });
-// const google = require('googleapis').google;
-// const customSearch = google.customsearch('v1');
-
-// const gcpCredentials = require('./credentials/gcp.json');
-
-// const response = customSearch.cse.list({
-//   auth: gcpCredentials.apiKey,
-//   cx: gcpCredentials.searchEngineId,
-//   q: 'Joel Collact',
-//   searchType: 'image',
-//   num: 10
-// });
-// response
-//   .then(data => {
-//     const { items } = data.data;
-//     const ArrImg = [];
-//     items.map(data => {
-//       ArrImg.push(data.link);
-//     });
-//     console.log(ArrImg);
-//   })
-//   .catch(e => console.log(e, 'ERROR'));
